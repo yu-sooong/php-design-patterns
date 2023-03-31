7725981258771f43fc190f3338e7a4c307f0e478
Ting <sscode543@gmail.com>
2023-03-31 16:07:20 +0800

    Add Enum & ExampleTest

---
+++ enum/AdventurerTypeName.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ting\Dp\enum;
+
+enum AdventurerTypeName: string
+{
+    /**
+     * 弓箭手
+     */
+    case ARCHER = 'Archer';
+
+    /**
+     * 戰士
+     */
+    case WARRIOR = 'Warrior';
+}
---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_successful()
+    {
+        $this->assertTrue(true);
+    }
+}
